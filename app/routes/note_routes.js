c5e9c60bfb796865b7c3d201ac5d79237115625f
Jason <jason.dubreuil@yahoo.com>
2017-12-12 15:03:38 -0500

    update
    update

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -3,14 +3,21 @@ const express        = require('express');
 const app            = express();
 const db             = require('./config/db');
 
+// note_routes.js
 var ObjectID = require('mongodb').ObjectID;
-var jsonValidation = require("json-validation");
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
 module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      }
+    });
+  });
+app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
