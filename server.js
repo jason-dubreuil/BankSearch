cf979dc6ac85221e130e6e938a4743b030fa1ae4
Jason <jason.dubreuil@yahoo.com>
2017-12-12 13:57:19 -0500

    port change
    port change

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-// server.js
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app            = express();
+const db             = require('./config/db');
+const validation     = require('json-validation');
 
-const port = 9000;
+app.use(bodyParser.json()); // support json encoded bodies
 
+const port = 8000;
 
-app.listen(port, () => {
-  console.log('We are live on ' + port);
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+  require('./app/routes')(app, database);
+  app.listen(port, () => {
+    console.log('We are live on YIpee!' + port);
+  });
+})
